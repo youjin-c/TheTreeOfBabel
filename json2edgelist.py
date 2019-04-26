6272dab57f01df8390d41c66a89085047be969a1
Youjin Chung <elqtfy@gmail.com>
2019-04-25 22:33:16 -0400

    init

---
+++ json2edgelist.py
@@ -0,0 +1,34 @@
+import glob
+import sys
+import re
+import os
+from pathlib import Path
+import json
+from collections import Counter
+import array as arr
+
+def refine(path):
+    for entry in os.scandir(path):
+        if entry.name.split('.')[0] .isdigit():
+            with open(entry,'rt') as jsonfile:#, open(entry.name.split('.')[0]+'.edgelist','w') as jf:
+                data = json.load(jsonfile)
+                print(data["edges"])
+                # for i, line in enumerate(fd):
+                    # for s in line.split():  
+                        # if not s.isdigit():
+                            # print(entry.name.split('.')[0],i,':',line)
+                    #############cut off more than third column ######
+                    # if len(line.split())>2:
+                        # print(entry.name,i,':',line,re.findall(r"[\w\d']+", line))
+                    #    print(entry.name,i,':',line,line.split(' ')[0:2])
+                    #    jf.write(line.rsplit('\t',1)[0])
+                    ##################################################
+                        
+                    # if not re.match("^[\d\s]+$",line): #[^0-9] #^[\d\s]+$ :numbers and space
+                        # print(entry.name,i,':',line)
+                        # jf.write(line.replace("E","1000"))
+                    # else:
+                        # pass
+                        # jf.write(line)
+
+refine(sys.argv[1])
