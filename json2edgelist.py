258e7651d81284f7bb2ae9c69cd32156f8f19122
Youjin <elqtfy@gmail.com>
2019-04-28 19:50:55 -0400

    Update json2edgelist.py

--- json2edgelist.py
+++ json2edgelist.py
@@ -5,8 +5,11 @@ import json
 
 import torch
 from torch_geometric.data import Data,InMemoryDataset,DataLoader
-from torch import Tensor as T
-from torch_geometric.nn import GAE, VGAE, ARGA, ARGVA
+
+import argparse
+import torch.nn.functional as F
+import torch_geometric.transforms as T
+from torch_geometric.nn import GCNConv, GAE, VGAE
 
 def datalist(path):
     data_list = []
@@ -33,15 +36,85 @@ loader = DataLoader(data_list,batch_size = 32,shuffle=False)
 #     print(data.x)
 #     print(data.edge_index)
 
+data = loader[0]
 
-# data = dataset[0]
-model = GAE(encoder=lambda x: x)
-model.reset_parameters()
-for data in loader:
-    z = model.encode(data.x)
-    # adj = model.decoder.forward_all(z)
-    value = model.decode(z, data.edge_index)
+parser = argparse.ArgumentParser()
+parser.add_argument('--model', type=str, default='GAE')
+args = parser.parse_args()
+assert args.model in ['GAE', 'VGAE']
+kwargs = {'GAE': GAE, 'VGAE': VGAE}
+
+
+class Encoder(torch.nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(Encoder, self).__init__()
+        self.conv1 = GCNConv(in_channels, 2 * out_channels, cached=True)
+        if args.model in ['GAE']:
+            self.conv2 = GCNConv(2 * out_channels, out_channels, cached=True)
+        elif args.model in ['VGAE']:
+            self.conv_mu = GCNConv(2 * out_channels, out_channels, cached=True)
+            self.conv_logvar = GCNConv(
+                2 * out_channels, out_channels, cached=True)
+
+    def forward(self, x, edge_index):
+        x = F.relu(self.conv1(x, edge_index))
+        if args.model in ['GAE']:
+            return self.conv2(x, edge_index)
+        elif args.model in ['VGAE']:
+            return self.conv_mu(x, edge_index), self.conv_logvar(x, edge_index)
+
+
+channels = 16
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model = kwargs[args.model](Encoder(loader.num_features, channels)).to(device)
+data.train_mask = data.val_mask = data.test_mask = data.y = None
+data = model.split_edges(data)
+x, edge_index = data.x.to(device), data.edge_index.to(device)###
+optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+
+
+def train():
+    model.train()
+    optimizer.zero_grad()
+    z = model.encode(x, edge_index)
+    loss = model.recon_loss(z, data.train_pos_edge_index)
+    if args.model in ['VGAE']:
+        loss = loss + 0.001 * model.kl_loss()
+    loss.backward()
+    optimizer.step()
+
+
+def test(pos_edge_index, neg_edge_index):
+    model.eval()
+    with torch.no_grad():
+        z = model.encode(x, edge_index)
+        value = model.decode(z, edge_index)
+    return model.test(z, pos_edge_index, neg_edge_index)
     print(value)
+
+
+for epoch in range(1, 201):
+    train()
+    auc, ap = test(data.val_pos_edge_index, data.val_neg_edge_index)
+    print('Epoch: {:03d}, AUC: {:.4f}, AP: {:.4f}'.format(epoch, auc, ap))
+
+auc, ap = test(data.test_pos_edge_index, data.test_neg_edge_index)
+print('Test AUC: {:.4f}, Test AP: {:.4f}'.format(auc, ap))
+
+# z = model.encode(x,edge_index)
+# value = model.decodee(z, edge_index)
+# print(value.tolist()) #assert value.tolist() == torch.sigmoid(torch.Tensor([-1, 4])).tolist()
+
+
+#######TESTAUTOENCODERFILE####################
+# data = dataset[0]
+# model = GAE(encoder=lambda x: x)
+# model.reset_parameters()
+# for data in loader:
+#     z = model.encode(data.x)
+#     # adj = model.decoder.forward_all(z)
+#     value = model.decode(z, data.edge_index)
+#     print(value)
     # print(data)
     # print(data.x)
     # print(data.edge_index)
