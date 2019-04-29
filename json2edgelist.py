3b6ea530d922efba65bbce122da2d34bd159c2a3
Youjin <elqtfy@gmail.com>
2019-04-29 01:52:09 -0400

    Update json2edgelist.py

--- json2edgelist.py
+++ json2edgelist.py
@@ -109,9 +109,9 @@ print('Test AUC: {:.4f}, Test AP: {:.4f}'.format(auc, ap))
 
 
 
-# z = model.encode(x,edge_index)
-# value = model.decodee(z, edge_index)
-# print(value.tolist()) #assert value.tolist() == torch.sigmoid(torch.Tensor([-1, 4])).tolist()
+z = model.encode(x,edge_index)
+value = model.decodee(z, edge_index)
+print(value.tolist()) #assert value.tolist() == torch.sigmoid(torch.Tensor([-1, 4])).tolist()
 
 
 #######TESTAUTOENCODERFILE####################
